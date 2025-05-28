d1abf941f3aa94944739de77f309b80e5732475b
Victor Koome <koomev786@gmail.com>
2025-05-28 21:37:11 +0300

    configure cors options

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,32 @@ const AuthUser = require('../models/authUser');
 const router = express.Router();
 
 
+router.post('/register', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if user already exists
+    const existingUser = await AuthUser.findOne({email});
+    if (existingUser) 
+        return res.status(400).json({ message: 'User already exists' });
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // Create a new user
+    const newUser = new AuthUser({
+      email,
+      password: hashedPassword,
+    }); 
+    // Save the user to the database
+    await newUser.save();       
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {     
+    console.error('Error registering user:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
+);
+    
+
 router.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body;
@@ -24,5 +50,7 @@ router.post('/login', async (req, res) => {
         res.status(500).json({error: error.message});
     }
 })
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 require('dotenv').config();
 const authRoutes = require('./routes/auth'); 
 
 const app = express();
 
+let corsOptions = {
+   origin : ['http://localhost:5173'],
+}
+
+app.use(cors(corsOptions)); 
 const port = process.env.PORT || 3001;
 app.use(express.json());
 
@@ -13,12 +19,12 @@ mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
-  console.log('Connected to MongoDB');
+  console.log('Connected to MongoDB');  
 }).catch(err => {
   console.error('MongoDB connection error:', err);
 });
 
-app.use('api/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
