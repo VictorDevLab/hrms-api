1efedc6d1c6ad99e3bd8c8a9ed6d6bb033ec767c
Victor Koome <koomev786@gmail.com>
2025-06-12 18:46:06 +0300

    update user controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -13,6 +13,17 @@ const getAllUsers = async (req, res) => {
         res.status(500).json({ message: 'Internal server error' });
      }
 }
+const getUserById = async (req, res) => {
+    try {
+        const userId = req.params.id
+        const user = await newUser.findById(userId)
+        if (!user) res.status(400).json({ message: "No user found" })
+        res.json(user)
+    } catch (error) {
+        console.error('Error fetching user:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
 const createUser = async (req, res) => {
     const { email, password, personal, employment, leaves, compensation } = req.body;
     if (!email || !password || !personal || !employment || !compensation) {
@@ -41,7 +52,7 @@ const createUser = async (req, res) => {
         // Generate a unique employee ID (ALPH- followed by count number based on the users collection)
         const userCount = await newUser.countDocuments();
         user.employment.employeeId = `ALPH-${userCount + 1}`;
-        //Add new properties to the user object(firstLogin..)
+        //TODO: Add new properties to the user object(firstLogin..)
         await user.save();
         res.status(201).json({ message: 'User created successfully' });
     } catch (error) {
@@ -50,7 +61,24 @@ const createUser = async (req, res) => {
     }
 };
 
+const updateUser = async (req, res) => {
+    try {
+        const { email, password, personal, employment, leaves, compensation } = req.body;
+        if (!email || !password || !personal || !employment || !compensation) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+        const userId = req.params.id
+        const user = await newUser.findByIdAndUpdate(userId, req.body, { new: true, runValidators: true })
+
+        if (!user) res.status(404).json({ message: 'No User Found!' })
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(400).json({ message: 'Failed to update user' })
+    }
+}
 module.exports = {
     createUser,
-    getAllUsers
+    getAllUsers,
+    updateUser,
+    getUserById
 }
